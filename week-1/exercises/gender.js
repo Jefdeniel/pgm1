511aca7e1ffed49b5f412a359e4a284caa687528
pgm-jefdenie <jef.deniel@student.arteveldehs.be>
2022-10-10 14:45:45 +0200

    Exercises week 1

---
+++ week-1/exercises/gender.js
@@ -0,0 +1,15 @@
+let genderCode;
+let genderCodeIs0 = "\n Je bent een man";
+
+switch (genderCode) {
+  case 0:
+    console.log(
+      "Gendercode = 1",
+      genderCodeIs0,
+      "background: #222; color: #bada55"
+    );
+    break;
+
+  default:
+    break;
+}
