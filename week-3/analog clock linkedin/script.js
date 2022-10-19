1dd7ea3e44bf8cc6e70d855113105506a98703c4
pgm-jefdenie <jef.deniel@student.arteveldehs.be>
2022-10-19 14:45:21 +0200

    clock exercise final

--- week-3/analog clock linkedin/script.js
+++ week-3/analog clock linkedin/script.js
@@ -2,9 +2,15 @@ const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
-let hrPosition = 20;
-let minPosition = 130;
-let secPosition = 280;
+function runTheClock() {
+  hrPosition = hrPosition + 30 / 3600;
+  minPosition = minPosition + 6 / 60;
+  secPosition = secPosition + 6;
+
+  HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
+  MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+  SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+}
 
 var date = new Date();
 console.log(date);
@@ -14,6 +20,8 @@ let sec = date.getSeconds();
 
 console.log("Hour: " + hr + " Minute: " + min + " Seconds: " + sec);
 
-HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
-MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
-SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+let hrPosition = (hr * 360) / 12 + (min * (360 / 60)) / 12;
+let minPosition = (min * 360) / 60 + (sec * (360 / 60)) / 60;
+let secPosition = (sec * 360) / 60;
+
+var interval = setInterval(runTheClock, 1000);
