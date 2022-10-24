f40376eb23fbce436f423fd762afea7f8b13d586
pgm-jefdenie <jef.deniel@student.arteveldehs.be>
2022-10-24 19:22:28 +0200

    push file srtucture

---
+++ week-5/DOM YT/script.js
@@ -0,0 +1,13 @@
+// Dom Manipulation
+
+// GetElementById()
+
+const title = document.GetElementById("main-heading");
+console.log(title);
+// GetElementByClassName()
+
+// GetElementByTagName()
+
+// querySelector()
+
+// querySelectorAll()
