3edcda8724c50b8609b9e1f29fc616ed48eca046
pgm-jefdenie <jef.deniel@student.arteveldehs.be>
2022-10-26 14:59:12 +0200

    add line-up data

--- opdracht_1_de_niel_jef/rockwerchter_app/app/index.js
+++ opdracht_1_de_niel_jef/rockwerchter_app/app/index.js
@@ -1,59 +1,144 @@
 const lineup = [
   {
-    id: "",
+    id: 1,
     band: {
-      name: "",
-      synopsis: "",
+      name: "Pearl Jam",
+      synopsis: "Pearl Jam stelt zijn volledige zomertournee 2021 uit naar de zomer van 2022. Het hele tourschema is ineens ook bekendgemaakt. En zo kent Rock Werchter 2022 zijn eerste naam. Op donderdag 30 juni 2022 is Pearl Jam de headliner op de openingsdag van het festival.",
       socials: {
-        website:"",
-        facebook:"",
-        twitter:"",
-        instagram:"", 
+        website:"https://pearljam.com/",
+        facebook:"https://www.facebook.com/pearljam",
+        twitter:"https://www.twitter.com/pearljam",
+        instagram:"https://www.instagram.com/pearljam", 
       },
       picture:{
-        small:"",
+        small:"https://assets.rockwerchter.be/files/cache/medium/files/14686-pearl-jam-032020-6c9a9964-flat-lowres-5f1a952633f63.jpg",
       },
       media: [
         {
-          link:"",
-          type:"",
+          link:"https://youtu.be/qM0zINtulhM",
+          //type:"youtube",
+        },
+        {
+          link:"https://www.youtube.com/watch?v=qM0zINtulhM",
+          // type:"youtube",
+        },
+        {
+          link:"https://upload.wikimedia.org/wikipedia/commons/6/69/PearlJam-Amsterdam-2012_%28cropped%29.jpg",
+          type:"image",
         }
       ]
     },
     place: {
-      name:"",
+      name:"Main Stage",
     },
-    from: "",
-    to: "",
-    isHeadliner: "",
+    from: "23.00",
+    to: "23.00",
+    isHeadliner: true,
     reviews: [
       {
-        rating: "", 
-        comment: "", 
-        reviewedAt: "",
+        rating: "9", 
+        comment: "First of all, wow! The show immediately grabs you with its first couple of scenes, letting you know that this show is going to have a more adult tone. Seeing the workings of everyday people in the galaxy, the perspective of an imperial loyalist, and a thief making his way through the galaxy is so thrilling and exciting. The big and little details you get out of all the varying cultures on different planets allows you to completely melt into and engross yourself in the Star Wars galaxy, which is what many of us love so much.", 
+        reviewedAt: "⭐⭐⭐⭐⭐⭐⭐⭐⭐",
+      },
+      {
+        rating: "8", 
+        comment: "Andor has proven to be my favorite of the Star Wars Disney+ series, because it simply provides the basics of storytelling. Characters have motivation and personality, the dialogue is smart and snappy, and the stakes feel personal and real. The production team has also done a wonderful job creating a world that looks and sounds blue collar - no Jedi or Sith, just billions of working persons trying to make a living and simply get by. All the flash of Star Wars - the lightsaber fights, space battles and blaster fights, come second to simply telling a decent story. And for that alone, I think Andor is a step in the right direction.", 
+        reviewedAt: "⭐⭐⭐⭐⭐⭐⭐⭐",
+      },
+      {
+        rating: "8", 
+        comment: "I Hope we could see more like this in the future... Disney have some problems with storylines from old characters but the news stories like mandalorian they nailed it... Please make this tone for the rest of the franchise. Star Wars should be a grown-up thing like most of the fans that grow up with. Stop wasting our time with current political agenda and give us timeless stories we can remember like the first movies from George Lucas. Thanks you Disney for letting this happen.", 
+        reviewedAt: "⭐⭐⭐⭐⭐⭐⭐⭐",
       }
     ],
     comments: [
       {
-        nickName: "",
-        message: "", 
+        nickName: "JaPPa03",
+        message: "In feite zijn deze camera’s een soort update die je doorgaans na interne feedback zou doorvoeren voordat je een product op de markt brengt. Vooral de dual card slot is iets wat ze verkeerd hebben ingeschat terwijl het geen nieuwe kritiek is op camera’s.", 
         comments: [
-          comment1: "",  
-          comment2: "",
-        ]
+          {
+          comment: "Precies dat idee heb ik ook, een gevalletje D600, 800 > 610, 810. An sich natuurlijk een prima camera en feitelijk (aannemend) beter dan de voorgangers maar een upgrade niet waard wanneer je met Nikon schiet en graag bij Nikon wilt blijven.",  
+          writtenBy: "Inoble"
+          }
+        ],
       }
     ]
-  }
+  },
+  {
+    id: 2,
+    band: {
+      name: "Altın Gün",
+      synopsis: "Jasper Verhulst koopt zich tijdens een Turkse toernee met de Nederlandse artiest Jacco Gardner arm in de tweedehands platenzaken in Istanbul. Hij zoekt en vindt er Turkse psychedelische folk uit de jaren 70. Klanken die uitgestorven zijn. Terug thuis wil Verhulst dat erfgoed nieuw leven inblazen. Hij zet een band op met twee zangers-muzikanten met Turkse roots en vier Nederlandse begeleiders. De naam is Altın Gün, Turks voor ‘gouden dag’. In november 2019 wordt hun tweede album ‘Gece’ genomineerd voor de Grammy Awards. Het levert uitnodigingen op voor grote festivals als Coachella en Fuji Rock. ‘Yol’ (2021) moest opgenomen worden in een idyllisch huis in Malibu. Het wordt helaas Amsterdam. De verplichte afzondering van de groepsleden laat zich horen in meer synthesizers en drummachines. Wat voor extra dansbaarheid zorgt", 
+      socials:{
+        website:"https://pearljam.com/",
+        facebook:"https://www.facebook.com/pearljam",
+        twitter:"https://www.twitter.com/altingunband",
+        instagram:"https://www.instagram.com/altingunban", 
+      },
+      picture:{
+        small:"https://assets.rockwerchter.be/files/cache/medium/files/14686-pearl-jam-032020-6c9a9964-flat-lowres-5f1a952633f63.jpg",
+      },
+      media: [
+        {
+          link:"https://youtu.be/QhDfOYn0118",
+          type:"youtube",
+        },
+        {
+          link:"https://festileaks.com/wp-content/uploads/2018/08/Alting-Gun-foto.jpg",
+          type:"image",
+        }
+      ]
+    },
+    place: {
+      name:"kLuB C",
+    },
+    from: "14.50",
+    to: "14.50",
+    isHeadliner: false,
+    reviews: [
+      {
+        rating: "9", 
+        comment: "I finally watched The Mandalorian after putting it off for so long and I absolutely loved it! I don't consider myself to be a huge Star Wars superfan like some of these other fans out there but I am a regular fan that really enjoys watching the movies. I didn't even mind the latest trilogy, Rogue One and Solo more than most but The Mandalorian is without a doubt the best thing Star Wars related since the original trilogy.", 
+        reviewedAt: "⭐⭐⭐⭐⭐⭐⭐⭐⭐",
+      },
+      {
+        rating: "7", 
+        comment: "I've been a fan of Star Wars since I was a child in the 1980s. As such, I was very disappointed with Disney's latest trilogy. Thankfully, shows like the Mandalorian manage to steer well clear of the atrociousness that those three films are.", 
+        reviewedAt: "⭐⭐⭐⭐⭐⭐⭐",
+      },
+      {
+        rating: "7", 
+        comment: "Another good spin-off series in an everlasting run of them it seems now. I've always realized that the Star Wars franchise is puffed up and placed with \"baby bumpers\" to be extra kid friendly. But a prevailing criticism I have with the ever continuous series of comic book movies and series and the Star Wars franchise: If you're going to have such adult themes like battle, war and killing(sometimes with decapitation?!), show some blood every once in a while?? Any color blood. My god, I just don't understand why it isn't acceptable for these creators to show this dude killing multiples of people as a bounty hunter, then never show any of the blood that accompanies killing someone", 
+        reviewedAt: "⭐⭐⭐⭐⭐⭐⭐",
+      },
+    ],
+    comments: [
+      {
+        nickName: "Nice, goede iteratie van de Z6 en Z7. Veel van de kritiekpunten zijn aangepakt",
+        message: "Joris748", 
+        comments: [
+          {
+          comment: "Als de APS-C crop voor 4K/60 op de Z6 geen probleem voor je is, is het zeker een mooie upgrade.",
+          writtenBy: "JaPPa03"  
+          },
+          {
+          comment: "In feite zijn deze camera’s een soort update die je doorgaans na interne feedback zou doorvoeren voordat je een product op de markt brengt. Vooral de dual card slot is iets wat ze verkeerd hebben ingeschat terwijl het geen nieuwe kritiek is op camera’s.",
+          writtenBy: "Inoble",  
+          },
+        ],
+      },
+    ],
+  },
 ];
 
 // IIFE //
 
-(function() {
-  // statements
-})();
+// (function() {
+//   // statements
+// })();
 
-// toe te passen functies: //
-function showLineupInConsole() {
-}
+// // toe te passen functies: //
+// function showLineupInConsole() {
+// }
 
 console.log(lineup)
