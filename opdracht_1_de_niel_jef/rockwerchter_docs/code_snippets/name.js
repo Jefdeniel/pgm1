3beb6ecff3bbb538fdbf0c8ec58346a2c3aacd6c
pgm-jefdenie <jef.deniel@student.arteveldehs.be>
2022-10-26 13:50:19 +0200

    index js

---
+++ opdracht_1_de_niel_jef/rockwerchter_docs/code_snippets/name.js
@@ -0,0 +1,6 @@
+const name = ["Pearl Jam", "Altin Gün"];
+
+const 
+
+const scheiding = "፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤፤\n";
+const groteScheiding = "_____________\n";
