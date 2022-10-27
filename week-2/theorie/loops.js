99782c658412625e61f36a170cd21da39a093f95
pgm-jefdenie <jef.deniel@student.arteveldehs.be>
2022-10-27 17:48:54 +0200

    change file structure

---
+++ week-2/theorie/loops.js
@@ -0,0 +1,11 @@
+//loops // 
+
+// eersrte stuk is je initiele expressie, daarna: wat je variabele minimaal of maximaal van waarde mag hebben
+
+// <=10 is dus tot 10 
+for(let i = 0; i <= 10; i++){
+console.log(i)
+}
+
+for (let j = 1000; j >=0; j++)
+console.log£(`counter with a value ${j}`)
