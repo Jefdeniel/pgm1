48c954f6d7fe8482425093296a5897dbaa816372
pgm-jefdenie <jef.deniel@student.arteveldehs.be>
2022-11-09 19:02:00 +0100

    dom on own website

--- week-4/first_website/js/main.js
+++ week-4/first_website/js/main.js
@@ -6,3 +6,18 @@ console.log(title);
 document.title = "my portfolio website";
 
 const $container = document.querySelector(".container");
+console.log($container);
+const txt = $container.innerText;
+console.log(txt);
+$container.innerText = "www";
+
+function tick() {
+  const date = new Date();
+  const timeStr = date.toTimeString().split(" ")[0];
+  $container.innerText = timeStr;
+  $container.style.transform = `rotate(${date.getSeconds() * 6})`;
+}
+
+setInterval(() => {
+  tick();
+}, 500);
