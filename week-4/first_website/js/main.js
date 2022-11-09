1899594d43cff991c15cafe3542adc000ee85a80
pgm-jefdenie <jef.deniel@student.arteveldehs.be>
2022-11-09 18:00:22 +0100

    week 4 vr nm 1.00.44

---
+++ week-4/first_website/js/main.js
@@ -0,0 +1,8 @@
+console.log("Hello world");
+
+console.log(document);
+const title = document.title;
+console.log(title);
+document.title = "my portfolio website";
+
+const $container = document.querySelector(".container");
