1316aa430e320a12602b2f51c5b9526a4ce2e488
pgm-jefdenie <jef.deniel@student.arteveldehs.be>
2022-11-13 20:48:03 +0100

    file structure and socials for last project

---
+++ OPDR2_afstudeerprojecten/app/js/data.js
@@ -0,0 +1,78 @@
+const socials = [
+  {
+    id: "Facebook",
+    logo: "",
+    link: "",
+  },
+  {
+    id: "Instragram",
+    logo: "",
+    link: "",
+  },
+  {
+    id: "Youtube",
+    logo: "",
+    link: "",
+  },
+];
+
+console.log(socials);
+
+const projects = [
+  {
+    image: "screenshot here",
+    name: "Charlotte Delvaux",
+    title: "Dialectische Gedragstherapie (DGT)",
+    languages: ["Angular", "NestJS"],
+  },
+  {
+    image: " ",
+    name: "Dylan Cathelijn",
+    title: "Buurtkajaks Gent",
+    languages: ["React Native", "Firebase"],
+  },
+  {
+    image: " ",
+    name: "Jan Deschacht",
+    title: "Tekst.ai",
+    languages: ["NextJs", "Strapi"],
+  },
+  {
+    image: " ",
+    name: "Thabisa Dinigani",
+    title: "Virtual Closet ",
+    languages: ["React Native", "Firebase"],
+  },
+  {
+    image: " ",
+    name: "Jamie-Lee Hart ",
+    title: "Car Expense ",
+    languages: ["ReactJS", "Supabase"],
+  },
+  {
+    image: " ",
+    name: "Crypto Tracker ",
+    title: "Aiden Soufi ",
+    languages: ["React Native", "Firebase"],
+  },
+  {
+    image: " "
+    name: "Nicolas Cnudde",
+    title: "Le Macarena",
+    languages: ["VueJs", "GraphQL"],
+  },
+  {
+    image: " "
+    name: "Bram Vandenbussche",
+    title: "Torchlight ",
+    languages: ["Angular", "Firebase"],
+  },
+  {
+    image: " "
+    name: "Bram Criel",
+    title: "DiscoverSound ",
+    languages: ["ReactJS", "Firebase"],
+  },
+];
+
+console.log(projects);
