0c154090396fb418199ee6b10bc0c9836aa1c93b
pgm-jefdenie <jef.deniel@student.arteveldehs.be>
2022-11-13 19:16:14 +0100

    rock werchter solution

--- week-6/demo-02/js/main.js
+++ week-6/demo-02/js/main.js
@@ -28,67 +28,43 @@ const concerts = [
 (() => {
   const app = {
     init() {
-      console.log("1. Application Initialized!");
+      console.log("1. Application Initialized");
       // Variables
-      // Call the function cacheElements
+      // call the funcion cacheElements
       this.cacheElements();
-      // Call the function generateUI
+      //Call the function generateUI
       this.generateUI();
     },
     cacheElements() {
-      console.log("2. Chache the elements!");
-      this.$concertsListDanceHall = document.querySelector(
-        ".stage--dancehall .lineup-stage__list"
-      );
-      this.$concertDetails = document.querySelector(".concert__details");
+      console.log("2. cache the elements");
+      // Get the element with class name concerts list
+      this.$concertsList = document.querySelector(".concerts__list");
     },
     generateUI() {
-      console.log("3. Generate User Interface!");
-      this.$concertsListDanceHall.innerHTML =
-        this.generateHtmlForConcerts(concerts);
-      // Add listeners to all listitems under de unordered list with class name .concerts__list
-      const $concerts =
-        this.$concertsListDanceHall.querySelectorAll(".concert");
+      console.log("3. Generate user interface");
+      this.$concertsList.innerHTML = this.generateHtmlForConcerts(concerts);
+      // add listeners to all listitems under the unordered list with class name .concerts__list
+      const $concerts = this.$concertsList.querySelectorAll(".concert");
       $concerts.forEach(($elem) => {
         $elem.addEventListener("click", (ev) => {
-          const elem = ev.currentTarget;
-          const id = elem.dataset.id;
-          //Get concert where id is equal to this id
-          const concert = concerts.find((concert) => concert.id === id);
-          this.$concertDetails.innerHTML =
-            this.generateHtmlForConcertDetails(concert);
+          console.log(this);
         });
       });
     },
+    // Hier schrijf je je eerste functies meestal
     generateHtmlForConcerts(concerts) {
       let tmpStr = "";
-      tmpStr += concerts
-        .map(
-          (concert) => `
+      tmpStr += concerts.map(
+        (concert) => `
         <li class="concert" data-id="${concert.id}">
-          ${concert.bandName}
+          ${concert.bandName} 
           <span>${this.getTimeFromCurrentTimeMillis(
             concert.from
-          )} - ${this.getTimeFromCurrentTimeMillis(concert.to)}</span>
+          )} -${this.getTimeFromCurrentTimeMillis(concert.to)}</span>
         </li>
       `
-        )
-        .join("");
+      ));
       return tmpStr;
-    },
-    generateHtmlForConcertDetails(concert) {
-      return `
-      <header>
-        <img src=${concert.thumbnail}>
-      </header>
-      <h3>${concert.bandName}</h3>
-      <span>${this.getTimeFromCurrentTimeMillis(
-        concert.from
-      )} - ${this.getTimeFromCurrentTimeMillis(concert.to)}</span>
-      <span>${concert.place}</span>
-      <span>${concert.genre}</span>
-      <p>${concert.biography}</p>
-      `;
     },
     getTimeFromCurrentTimeMillis(millis) {
       const date = new Date(millis);
---
+++ week-6/solutionwerchter/js/main.js
@@ -0,0 +1,158 @@
+(() => {
+  const app = {
+    init() {
+      console.log("1. Initialize Application");
+      this.cacheElements();
+      this.generateUI();
+    },
+    cacheElements() {
+      console.log("2. Cache Elements");
+      this.$lineup = document.querySelector(".lineup");
+      this.$concertDetails = document.querySelector(".concert-details");
+      this.$digitalClock = document.querySelector(".digital-clock");
+    },
+    generateUI() {
+      console.log("3. Generate User Interface");
+      this.$lineup.innerHTML = this.generateHTMLForLineup(lineup);
+      const $concerts = this.$lineup.querySelectorAll(".concert");
+      $concerts.forEach(($concert) => {
+        $concert.addEventListener("click", (ev) => {
+          const id = ev.currentTarget.dataset.id;
+          this.generateUIForConcertDetails(id);
+        });
+      });
+      this.clockInterval = setInterval(() => {
+        this.updateDigitalClock();
+      }, 500);
+    },
+    generateHTMLForLineup(concerts) {
+      let tmpStr = "";
+      for (let i = 0; i < concerts.length; i++) {
+        tmpStr += this.generateHTMLForConcert(concerts[i]);
+      }
+      return tmpStr;
+    },
+    generateHTMLForConcert(concert) {
+      return `
+      <div class="concert" data-id="${concert.id}">
+        <h2 class="concert__band">${concert.band.name}</h2>
+        <picture class="concert__picture">
+          <img src="${concert.band.picture.small}">
+        </picture>
+        <div class="concert__meta">
+          <span class="date__from">${new Date(
+            concert.from
+          ).toDateString()}</span>
+        </div>
+      </div>
+      `;
+    },
+    generateUIForConcertDetails(id) {
+      const concert = lineup.find((concert) => concert.id === id);
+      this.$concertDetails.innerHTML =
+        this.generateHTMLForConcertDetails(concert);
+    },
+    generateHTMLForConcertDetails(concert) {
+      console.log(concert);
+      return `
+      <div class="concert-details__left">
+        <picture class="concert-details__picture">
+          <img src="${concert.band.picture.small}">
+        </picture>
+      </div>
+      <div class="concert-details__right">
+        <div class="concert-details__meta">${new Date(
+          concert.from
+        ).toDateString()}</div>
+        <h1 class="concert-details__bandname">${concert.band.name}</h1>
+        <ul class="socials__list">
+          ${this.generateHTMLForSocials(concert.band.socials)}
+        </ul>
+        <p class="concert-details__synopsis">${concert.band.synopsis}</p>
+        <div class="concert-details__media">
+          ${this.generateHTMLForMedia(concert.band.media)}
+        </div>
+        <div class="concert-details__reviews">
+          <h3 class="concert-details__reviews-title">Reviews</h3>
+          <div class="concert-details__reviews-list">
+            ${this.generateHTMLForReviews(concert.reviews)}
+          </div>
+        </div>
+      </div>
+      `;
+    },
+    generateHTMLForSocials(socials) {
+      let tmpStr = "";
+      for (let prop in socials) {
+        tmpStr += `<li class="socials__list-item"><a href="${
+          socials[prop]
+        }" target="__blank" rel="noreferrer nofollow">${this.generateHTMLForSocialMediaIcon(
+          prop
+        )}</i><span>${prop}</span></a></li>`;
+      }
+      return tmpStr;
+    },
+    generateHTMLForSocialMediaIcon(socialMedia) {
+      switch (socialMedia) {
+        case "website":
+        default:
+          return '<i class="bi bi-globe">';
+        case "youtube":
+          return '<i class="bi bi-youtube">';
+        case "spotify":
+          return '<i class="bi bi-spotify">';
+        case "facebook":
+          return '<i class="bi bi-facebook">';
+        case "instagram":
+          return '<i class="bi bi-instagram">';
+        case "twitter":
+          return '<i class="bi bi-twitter">';
+      }
+    },
+    generateHTMLForMedia(media) {
+      return media
+        .map((medium) => {
+          let tmpStr = "";
+          switch (medium.type) {
+            case "youtube":
+              tmpStr = `
+          <iframe width="100%" height="395" src="https://www.youtube.com/embed/${medium.link}" title="MUSE - Blockades [Official Music Video]" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          `;
+              break;
+            case "image":
+            default:
+              tmpStr = `
+          <picture class="concert-details__medium-picture">
+            <img src="${medium.link}">
+          </picture>
+          `;
+              break;
+          }
+          return tmpStr;
+        })
+        .join("");
+      return tmpStr;
+    },
+    updateDigitalClock() {
+      this.$digitalClock.innerHTML = new Date().toLocaleTimeString();
+    },
+    generateHTMLForReviews(reviews) {
+      return reviews
+        .map((review) => {
+          return `
+        <div class="concert-details__reviews-list-item">
+          <div class="review__meta">
+            <span class="review__rating">${"🌟".repeat(review.rating)}</span>
+            <span class="review__date">${new Date(
+              review.reviewedAt
+            ).toDateString()}</span>
+          </div>
+          <p class="review__comment">${review.comment}</p>
+        </div>
+        `;
+        })
+        .join("");
+    },
+  };
+  app.init();
+})();
